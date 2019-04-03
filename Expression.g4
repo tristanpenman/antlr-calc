d2a433c7515e343c415320f24026a40a4a01c35d
Tristan Penman <tristan@tristanpenman.com>
2019-04-03 16:10:34 +1100

    Fully evaluate expressions using a visitor

--- Expression.g4
+++ Expression.g4
@@ -1,10 +1,20 @@
 grammar Expression;
 
 main: expr EOF;
-expr: expr ('*'|'/') expr
-    | expr ('+'|'-') expr
-    | INT
-    | '(' expr ')'
+expr: expr op=(MUL|DIV) expr  # multiplicativeExpr
+    | expr op=(ADD|SUB) expr  # additiveExpr
+    | LITERAL                 # literalExpr
+    | LPAREN expr RPAREN      # bracketedExpr
     ;
 
-INT : [0-9]+ ;
+ADD : '+' ;
+DIV : '/' ;
+MUL : '*' ;
+SUB : '-' ;
+
+LPAREN : '(' ;
+RPAREN : ')' ;
+
+LITERAL : '-'?('0'..'9')+('.'('0'..'9')+)? ;
+
+WS : [ \r\n\t] + -> skip ;
--- calculator.cpp
+++ calculator.cpp
@@ -5,10 +5,54 @@
 
 #include "generated/ExpressionLexer.h"
 #include "generated/ExpressionParser.h"
+#include "generated/ExpressionVisitor.h"
+
+class MyExpressionVisitor : public ExpressionVisitor
+{
+public:
+    antlrcpp::Any visitMain(ExpressionParser::MainContext *ctx)
+    {
+        return visit(ctx->expr());
+    }
+
+    antlrcpp::Any visitLiteralExpr(ExpressionParser::LiteralExprContext *ctx)
+    {
+        return std::atof(ctx->LITERAL()->getText().c_str());
+    }
+
+    antlrcpp::Any visitAdditiveExpr(ExpressionParser::AdditiveExprContext *ctx)
+    {
+        const auto left = visit(ctx->expr(0)).as<double>();
+        const auto right = visit(ctx->expr(1)).as<double>();
+
+        if (ctx->op->getType() == ExpressionParser::ADD) {
+            return left + right;
+        }
+
+        return left - right;
+    }
+
+    antlrcpp::Any visitMultiplicativeExpr(ExpressionParser::MultiplicativeExprContext *ctx)
+    {
+        const auto left = visit(ctx->expr(0)).as<double>();
+        const auto right = visit(ctx->expr(1)).as<double>();
+
+        if (ctx->op->getType() == ExpressionParser::MUL) {
+            return left * right;
+        }
+
+        return left / right;
+    }
+
+    antlrcpp::Any visitBracketedExpr(ExpressionParser::BracketedExprContext *ctx)
+    {
+        return visit(ctx->expr());
+    }
+};
 
 class MyParserErrorListener: public antlr4::BaseErrorListener
 {
-    virtual void syntaxError(
+    void syntaxError(
         antlr4::Recognizer * recognizer,
         antlr4::Token * offendingSymbol,
         size_t line,
@@ -27,7 +71,9 @@ int main()
     while (std::cin) {
         std::cout << "> ";
         std::string input;
-        std::getline(std::cin, input);
+        if (!std::getline(std::cin, input)) {
+            break;
+        }
         
         antlr4::ANTLRInputStream inputStream(input);
         ExpressionLexer lexer(&inputStream);
@@ -38,13 +84,17 @@ int main()
         ExpressionParser parser(&tokens);
         parser.removeErrorListeners();
         parser.addErrorListener(&errorListner);
+
         try {
-            antlr4::tree::ParseTree* tree = parser.main();
-            std::cout << tree->toStringTree() << std::endl;
+            ExpressionParser::MainContext* tree = parser.main();
+            MyExpressionVisitor visitor;
+            std::cout << visitor.visitMain(tree).as<double>() << std::endl;
         } catch (std::invalid_argument &e) {
             std::cout << e.what() << std::endl;
         }
     }
+
+    std::cout << std::endl;
 
     return 0;
 }
